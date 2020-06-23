cb6d29bfcd946aa953432ea57f853bcc3fae7ddc
Tim Cvetko <52534783+Timothy102@users.noreply.github.com>
2020-06-23 16:15:22 +0200

    Add files via upload

---
+++ matrix.go
@@ -0,0 +1,410 @@
+//Package matrix implements matrix and vector operations along with some difficult methods, the Gram-Schmidt process, eigenvector calculation and more.
+
+package matrix
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"math/rand"
+	"time"
+)
+
+//Matrix type does matrix math
+type Matrix struct {
+	slice [][]float64
+}
+
+//NewMatrix returns a matrix and an error
+func NewMatrix(slice [][]float64) Matrix {
+	mat := Matrix{slice: slice}
+	rows := sliceRows(slice)
+	columns := sliceColumns(slice)
+
+	if columns < 2 || rows < 2 {
+		log.Fatalf("This is not a matrix. Please, enter a proper number of elements.")
+	}
+
+	return mat
+}
+
+//helper functions
+func sliceColumns(slice [][]float64) int {
+	return len(slice[len(slice)-1])
+}
+
+//helper functions
+func sliceRows(slice [][]float64) int {
+	return len(slice)
+}
+
+//NumberOfColumns returns the number of columns.
+func (m Matrix) NumberOfColumns() int {
+	return sliceColumns(m.slice)
+}
+
+//NumberOfRows returns the number of columns.
+func (m Matrix) NumberOfRows() int {
+	return sliceRows(m.slice)
+}
+
+//Dimensions returns the number of rows and columns of m.
+func (m Matrix) Dimensions() (int, int) {
+	return m.NumberOfRows(), m.NumberOfColumns()
+}
+
+//NumberOfElements returns the number of elements.
+func (m Matrix) NumberOfElements() int {
+	return m.NumberOfColumns() * m.NumberOfRows()
+}
+
+//RoundtoDecimals round all elements of a matrix to decimals accuracy.
+func (m Matrix) RoundtoDecimals(decimals int) Matrix {
+	for _, r := range m.slice {
+		for _, e := range r {
+			e = roundTo(e, decimals)
+		}
+	}
+	return m
+}
+func roundTo(number float64, decimals int) float64 {
+	s := math.Pow(10, float64(decimals))
+	return math.Round(number*s) / s
+}
+
+//Randomize randomizes m to random values
+func (m Matrix) Randomize() Matrix {
+	row := m.NumberOfRows()
+	column := m.NumberOfColumns()
+	for i := 0; i < row; i++ {
+		for j := 0; j < column; j++ {
+			m.slice[i][j] = rand.Float64() * 000.3
+		}
+	}
+	return m
+}
+
+//RandomValuedMatrix returns a row*column random valued matrix.
+func RandomValuedMatrix(row, column int) Matrix {
+	rand.Seed(time.Now().UnixNano())
+	m := Zeros(row, column)
+	for i := 0; i < row; i++ {
+		for j := 0; j < column; j++ {
+			m.slice[i][j] = rand.Float64()
+		}
+	}
+	return m
+}
+func randFloats(n int) []float64 {
+	rand.Seed(time.Now().UnixNano())
+	fls := make([]float64, n)
+	for i := range fls {
+		fls[i] = rand.Float64()
+	}
+	return fls
+}
+
+//Slice returns matrix.slice
+// You can perform indexing with this method.
+func (m Matrix) Slice() [][]float64 {
+	return m.slice
+}
+
+//PrintByRow prints the matrix by row.
+func (m Matrix) PrintByRow() {
+	for r := range m.slice {
+		fmt.Println(m.slice[r])
+	}
+}
+
+//At method finds the value at rowIndex,columnIndex
+func (m *Matrix) At(rowIndex, columnIndex int) float64 {
+	return m.slice[rowIndex][columnIndex]
+}
+
+//Identity function returns an n*n identity matrix
+func Identity(n int) Matrix {
+	matrix := Matrix{}
+	k := 0
+	for i := 0; i < n; i++ {
+		slice := make([]float64, n)
+		slice[k] = 1
+		k++
+		matrix.slice = append(matrix.slice, slice)
+	}
+	return matrix
+}
+
+//Zeros returns a matrix of zeros.
+func Zeros(row, column int) Matrix {
+	b := make([][]float64, row)
+	v := make([]float64, column)
+	for i := 0; i < row; i++ {
+		for j := 0; j < column; j++ {
+			v[j] = 0
+			b[i] = v
+		}
+	}
+
+	return Matrix{slice: b}
+}
+
+//Ones returns a matrix of ones.
+func Ones(row, column int) Matrix {
+	b := make([][]float64, row)
+	v := make([]float64, column)
+	for i := 0; i < row; i++ {
+		for j := 0; j < column; j++ {
+			v[j] = 1
+			b[i] = v
+		}
+	}
+
+	return Matrix{slice: b}
+}
+
+//AllSameNumber returns a row * column matrix of number.
+func AllSameNumber(row, column int, number float64) Matrix {
+	b := make([][]float64, row)
+	v := make([]float64, column)
+	for i := 0; i < row; i++ {
+		for j := 0; j < column; j++ {
+			v[j] = number
+			b[i] = v
+		}
+	}
+
+	return Matrix{slice: b}
+}
+
+//MatrixFromValues returns a matrix from a set of values
+func MatrixFromValues(row, column int, values []float64) Matrix {
+	slice := make([][]float64, row)
+	for i := range values {
+		if (i+1)%column == 0 {
+			slc := make([]float64, column)
+			slc = append(slc, values[i+1], values[i+2], values[i+2])
+			slice = append(slice, slc)
+		}
+	}
+	return Matrix{slice: slice}
+}
+
+//Add performs elementary matrix addition
+func (m Matrix) Add(mat Matrix) Matrix {
+	var product Matrix
+	for i := 0; i < m.NumberOfRows(); i++ {
+		for j := 0; j < m.NumberOfColumns(); j++ {
+			product.slice[i][j] = m.slice[i][j] + mat.slice[i][j]
+		}
+	}
+	return product
+}
+
+//Subtract performs elementary matrix subtraction
+func (m Matrix) Subtract(mat Matrix) Matrix {
+	var product Matrix
+	for i := 0; i < m.NumberOfRows(); i++ {
+		for j := 0; j < m.NumberOfColumns(); j++ {
+			product.slice[i][j] = m.slice[i][j] - mat.slice[i][j]
+		}
+	}
+	return product
+}
+
+//Multiply performs elementary matrix multiplication
+func (m Matrix) Multiply(mat Matrix) Matrix {
+	var product Matrix
+	for i := 0; i < m.NumberOfRows(); i++ {
+		for j := 0; j < m.NumberOfColumns(); j++ {
+			product.slice[i][j] = m.slice[i][j] * mat.slice[i][j]
+		}
+	}
+	return product
+}
+
+//Divide performs elementary matrix division
+func (m Matrix) Divide(mat Matrix) Matrix {
+	var product Matrix
+	for i := 0; i < m.NumberOfRows(); i++ {
+		for j := 0; j < m.NumberOfColumns(); j++ {
+			product.slice[i][j] = m.slice[i][j] / mat.slice[i][j]
+		}
+	}
+	return product
+}
+
+//ScalarMultiplication multiplies every element with a scalar
+func (m Matrix) ScalarMultiplication(scalar float64) Matrix {
+	for _, r := range m.slice {
+		for i := range r {
+			r[i] = r[i] * scalar
+		}
+	}
+	return m
+}
+
+//ScalarAdition adds a scalar to every elements
+func (m Matrix) ScalarAdition(scalar float64) Matrix {
+	for _, r := range m.slice {
+		for i := range r {
+			r[i] = r[i] + scalar
+		}
+	}
+	return m
+}
+
+//Transpose returns the tranpose of a matrix
+func (m Matrix) Transpose() Matrix {
+	for i, r := range m.slice {
+		for j := range r {
+			m.slice[i][j] = m.slice[j][i]
+		}
+	}
+	return m
+}
+
+//FindDeterminant returns the matrix determinant
+func (m Matrix) FindDeterminant() float64 {
+	dims := m.NumberOfRows()
+	var determinant, p float64
+	for k := 0; k < dims; k++ {
+		if k%2 == 0 {
+			p = -1.0
+		} else {
+			p = 1.0
+		}
+		if dims == 1 {
+			log.Fatalf("This is a single valued matrix.")
+		} else if dims == 2 {
+			determinant += m.slice[0][k] * m.Shorten(0, k).Find2x2Determinant() * p
+		} else {
+			determinant += m.slice[0][k] * m.Shorten(0, k).FindDeterminant() * p
+		}
+	}
+	return determinant
+}
+
+//Find2x2Determinant returns the determinant of a 2x2 matrix
+func (m Matrix) Find2x2Determinant() float64 {
+	return m.slice[0][0]*m.slice[1][1] - m.slice[1][0]*m.slice[1][0]
+}
+
+//Shorten returns the so-called minor matrix, it shrinks all numbers that lie either with one coordinate on rowIndex or columnIndex
+func (m Matrix) Shorten(rowIndex, columnIndex int) Matrix {
+	for j, r := range m.slice {
+		for i := range r {
+			m.slice[rowIndex][j] = 0.0
+			m.slice[i][columnIndex] = 0.0
+			m.slice[i][j] = m.slice[i-1][j-1]
+		}
+	}
+	return m
+}
+
+//Adjoint returns the adjoint matrix
+func (m Matrix) Adjoint() (Matrix, error) {
+	for i, r := range m.slice {
+		for j := range r {
+			m.slice[i][j] = math.Pow(-1, float64(i+j)) * m.Shorten(i, j).FindDeterminant()
+		}
+	}
+	return m, nil
+}
+
+//Inverse returns the inverse of a matrix
+func (m Matrix) Inverse() Matrix {
+	var inverse Matrix
+	det := m.FindDeterminant()
+	adjoint, err := m.Adjoint()
+	if err != nil {
+		log.Fatalf("unable to create adjoint matrix :%v", err)
+	}
+	inverse = adjoint.ScalarMultiplication(1 / det)
+	return inverse
+}
+
+//Inverse2x2 returns the inverse of a 2x2 matrix
+func (m Matrix) Inverse2x2() Matrix {
+	if m.NumberOfRows() != 2 {
+		log.Fatalf("This is not a 2x2 matrix.")
+	}
+	var result Matrix
+	result.slice[0][0] = m.slice[1][1]
+	result.slice[1][1] = m.slice[0][0]
+	result.slice[0][1] = -m.slice[0][1]
+	result.slice[1][0] = -m.slice[1][0]
+	return result
+}
+
+//EinsteinConvention returns the multiplication matrix of two matrices, given that rows of A matches columns of B.
+//According to this convention, when an index variable appears twice in a single term and is not otherwise defined, it implies summation of that term over all the values of the index.
+func (m Matrix) EinsteinConvention(m2 Matrix) Matrix {
+	var result Matrix
+	sum := 0
+	for range m2.slice {
+		sum++
+	}
+	if len(m.slice) != sum {
+		log.Fatal("Rows of A must match columns of B")
+	}
+	for n := 0; n < sum; n++ {
+		for h := 0; h < len(m.slice); h++ {
+			for i := 0; i < sum; i++ {
+				for j := 0; j < len(m2.slice); j++ {
+					result.slice[n][h] += m.slice[2][i] * m2.slice[j][3]
+				}
+			}
+		}
+	}
+	return result
+}
+
+//DotProduct returns the dot product of two matrices
+func (m Matrix) DotProduct(m2 Matrix) float64 {
+	var sum float64
+	for i := 0; i < m.NumberOfRows(); i++ {
+		for j := 0; j < m.NumberOfColumns(); j++ {
+			sum += m.slice[i][j] * m2.slice[i][j]
+		}
+	}
+	return sum
+}
+
+//FromArray returns a matrix from array
+func FromArray(arr []float64) Matrix {
+	m := Zeros(len(arr), 1)
+	for i := 0; i < len(arr); i++ {
+		m.slice[i][0] = arr[0]
+	}
+	return m
+}
+
+//ToArray returns the matrix in array form.
+func (m Matrix) ToArray() []float64 {
+	var arr []float64
+	for i := 0; i < m.NumberOfRows(); i++ {
+		for j := 0; j < m.NumberOfColumns(); j++ {
+			arr = append(arr, m.slice[i][j])
+		}
+	}
+	return arr
+}
+
+//MapFunc applies f to every element
+func (m Matrix) MapFunc(f func(x float64) float64) Matrix {
+	for i := 0; i < m.NumberOfRows(); i++ {
+		for j := 0; j < m.NumberOfColumns(); j++ {
+			m.slice[i][j] = f(m.slice[i][j])
+		}
+	}
+	return m
+}
+
+//TransformationInAChangedBasis function takes a given matrix as an input and outputs it in a changed basis
+func (m Matrix) TransformationInAChangedBasis(basis Matrix) Matrix {
+	inv := basis.Inverse()
+	transform := inv.Multiply(m)
+	result := transform.Multiply(basis)
+	return result
+}
---
+++ vector.go
@@ -0,0 +1,219 @@
+package matrix
+
+import (
+	"fmt"
+	"log"
+	"math"
+)
+
+type Vector struct {
+	row []float64
+}
+
+//NewVector returns a vector type
+func NewVector(slice []float64) Vector {
+	return Vector{row: slice}
+}
+
+// PrintVector prints the vector components
+func PrintVector(vec Vector) {
+	fmt.Println(vec.row)
+}
+
+// InnerProduct returns the inner product of two vectors via the matrix.
+// Check the dotProduct function for a simplified dot product of two vectors.
+func InnerProduct(matrix Matrix, vector1, vector2 Vector) float64 {
+	var product Vector
+	for _, r := range matrix.slice {
+		for i := 0; i < len(r); i++ {
+			product.row[i] = r[i] * vector1.row[i]
+		}
+	}
+	return vector2.DotProduct(product)
+}
+
+func (v Vector) NumberOfElements() int {
+	var sum int
+	for i := 0; i < len(v.row); i++ {
+		sum += i
+	}
+	return sum
+}
+
+// Slice returns vector.slice.
+// You can perform indexing with this method.
+func (v Vector) Slice() []float64 {
+	return v.row
+}
+
+//DotProduct returns the dot product of two vectors.
+func (v Vector) DotProduct(v2 Vector) float64 {
+	var sum float64
+	for i := 0; i < len(v.row); i++ {
+		sum += v.row[i] * v2.row[i]
+	}
+	return sum
+}
+
+//ApplyMatrix returns the vector through a matrix transformation.
+func (v Vector) ApplyMatrix(matrix Matrix) Vector {
+	var product Vector
+	for _, r := range matrix.slice {
+		for i := 0; i < len(r); i++ {
+			product.row[i] = r[i] * v.row[i]
+		}
+	}
+	return product
+}
+
+//Add returns an elementary operation on two vectors.
+func (v Vector) Add(v2 Vector) Vector {
+	var resultVector Vector
+	for i := 0; i < len(v.row); i++ {
+		resultVector.row[i] = v.row[i] + v2.row[i]
+	}
+	return resultVector
+}
+
+//Substract returns an elementary operation on two vectors.
+func (v Vector) Substract(v2 Vector) Vector {
+	var resultVector Vector
+	for i := 0; i < len(v.row); i++ {
+		resultVector.row[i] = v.row[i] - v2.row[i]
+	}
+	return resultVector
+}
+
+//AddMany takes a slice of vectors and outputs their sum.
+func (v Vector) AddMany(vectors []Vector) Vector {
+	var resultVector Vector
+	for _, vec := range vectors {
+		for i := 0; i < len(v.row); i++ {
+			resultVector.row[i] = v.row[i] + vec.row[i]
+		}
+	}
+	return resultVector
+}
+
+//SubstractMany takes a slice of vectors and outputs their divergence.
+func (v Vector) SubstractMany(vectors []Vector) Vector {
+	var resultVector Vector
+	for _, vec := range vectors {
+		for i := 0; i < len(v.row); i++ {
+			resultVector.row[i] = v.row[i] - vec.row[i]
+		}
+	}
+	return resultVector
+}
+
+//GetLength returns the length of a vector.
+func (v Vector) GetLength() float64 {
+	var result float64
+	for _, g := range v.row {
+		result += math.Pow(g, 2)
+	}
+	return math.Sqrt(result)
+}
+
+//AngleBetween returns the angle between two vectors.
+func (v Vector) AngleBetween(v2 Vector) float64 {
+	return math.Cos(v.DotProduct(v2) / (v.GetLength() * v2.GetLength()))
+}
+
+//ScalarProjection returns the scalar projection of v onto v2.
+func (v Vector) ScalarProjection(v2 Vector) float64 {
+	x := v2.GetLength() * v.AngleBetween(v2)
+	return x
+}
+
+//MultiplyByScalar operates by adding a scalar elementary.
+func (v Vector) MultiplyByScalar(scalar float64) Vector {
+	for _, g := range v.row {
+		g = g * scalar
+	}
+	return v
+}
+
+//VectorProjection returns the vector projection of v onto v2.
+func (v Vector) VectorProjection(v2 Vector) Vector {
+	x := v.DotProduct(v2)
+	y := v2.DotProduct(v2)
+	return v.MultiplyByScalar(x / y)
+}
+
+//ChangingBasis returns the transformed vector if we were to change its basis vectors.
+//Similar to a simple matrix transformation but inputs are two basis vectors.
+//Basis vectors have to be perpendicular to each other in order to apply this transformation.
+//The function takes care of this for you by sending back an error
+func (v Vector) ChangingBasis(b1, b2 Vector) (Vector, error) {
+	var newVector Vector
+	var db1, db2 float64
+	if b1.AngleBetween(b2) != 0 {
+		return Vector{}, fmt.Errorf("basis vectors have to be perpendicular")
+	}
+	db1 = v.DotProduct(b1) / math.Pow(b1.GetLength(), 2)
+	db2 = v.DotProduct(b2) / math.Pow(b2.GetLength(), 2)
+
+	newVector.row[0] = db1
+	newVector.row[1] = db2
+
+	return newVector, nil
+}
+
+//GramSchmidt function returns the concatenated matrix of orthogonal based vectors out of the vectors dataset.
+//Based on the Mathematics for Machine learning Specialization.
+func GramSchmidt(vectors []Vector) ([]Vector, error) {
+	var normalizedvectors []Vector
+	var xs []Vector
+
+	for i, vector := range vectors {
+		normalized := vectors[i-1].MultiplyByScalar(1 / vector.GetLength())
+		if i != 1 {
+			s := vectors[i].DotProduct(normalized)
+			x := normalized.MultiplyByScalar(s)
+			xs = append(xs, x)
+			normalizedvectors[i] = vectors[i].Substract(vectors[i].SubstractMany(xs))
+		}
+	}
+	return normalizedvectors, nil
+}
+
+//PageRank algorithm returns the probabilites of randomly ending up on either of the pages.
+//Page connections should be put inside the link matrix.
+func PageRank(linkMatrix Matrix, pages int) {
+	slice := make([]float64, pages)
+	for i := range slice {
+		slice[i] = 1 / float64(pages)
+	}
+	initVector := Vector{row: slice}
+	for i := 0; i < len(initVector.row); i++ {
+		initVector = initVector.ApplyMatrix(linkMatrix)
+	}
+}
+
+//CalculateEigenvectors2x2 returns the eigenvectors of the given 2x2 matrix
+func CalculateEigenvectors2x2(m Matrix) ([]Vector, error) {
+	var vectors []Vector
+	q := m.slice
+	det := m.Find2x2Determinant()
+	lambda1, lambda2 := Quadratic(1, q[0][0]+q[1][1], det)
+	vec1 := []float64{lambda1, 0}
+	vec2 := []float64{0, lambda2}
+	vector1 := Vector{row: vec1}
+	vector2 := Vector{row: vec2}
+
+	vectors = append(vectors, vector1)
+	vectors = append(vectors, vector2)
+	return vectors, nil
+}
+
+//Quadratic takes a,b,c parameters of a quadratic equation as inputs and returns both solutions via the discriminant.
+func Quadratic(a, b, c float64) (float64, float64) {
+	disc := math.Pow(b, 2) - 4*a*c
+	if disc < 0 {
+		log.Fatalf("The discriminant is negative. Therefore solutions will be complex numbers. ")
+	}
+	x1 := (-b + math.Sqrt(disc)) / 2 * a
+	x2 := (-b - math.Sqrt(disc)) / 2 * a
+	return x1, x2
+}
